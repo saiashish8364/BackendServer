3c3f7d19a81f613a904d4b0e3687e7ff985b77b2
saiashish8364 <polusaiashish97@gmail.com>
2023-10-29 17:16:54 +0530

    2nd

--- server/app.js
+++ server/app.js
@@ -1,8 +1,38 @@
 const http = require("http");
 
 function rqListner(req, res) {
-  console.log("Ashish");
+  const url = req.url;
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>hello</title></head>");
+    res.write("<body><h1>welcome home</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>hello</title></head>");
+    res.write("<body><h1>welcome to about</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>hello</title></head>");
+    res.write("<body><h1>welcome to my node js project</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>hello</title></head>");
+    res.write("<body><h1>welcome</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+
+  //process.exit();
 }
 const server = http.createServer(rqListner);
 
-server.listen(4000);
+server.listen(3000);
