874296b1f49b0e5c5c4382f1fdf1be46e833c65e
saiashish8364 <polusaiashish97@gmail.com>
2023-10-29 19:32:22 +0530

    3rd

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const http = require("http");
+const fs = require("fs");
+const { buffer } = require("stream/consumers");
 
 function rqListner(req, res) {
   const url = req.url;
+  const method = req.method;
   if (url === "/home") {
     res.write("<html>");
     res.write("<head><title>hello</title></head>");
@@ -24,11 +27,35 @@ function rqListner(req, res) {
     res.end();
   }
   if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>hello</title></head>");
-    res.write("<body><h1>welcome</h1></body>");
-    res.write("</html>");
-    res.end();
+    const path = "D:/git/Backend/createServer/server/message.txt";
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.write("<html>");
+      res.write("<head><title>hello</title></head>");
+      res.write(
+        `<body><p>${data}<form action='/message' method='POST' ><input type='text' name='message'/><button type='submit'>send</button></body>`
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 
   //process.exit();
